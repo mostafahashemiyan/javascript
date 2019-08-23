1a5347efb86b25db1c0f9cfdb29ae8c6e88a78d4
mostafahashemiyan <49098593+mostafahashemiyan@users.noreply.github.com>
2019-08-23 20:19:04 +0430

    Add files via upload

---
+++ project2/script.js
@@ -0,0 +1,22 @@
+var john=(89+120+103)/3
+var mike=(116+94+123)/3
+var mary=(97+134+105)/3
+console.log(john,mike,mary)
+if (john>mike && john>mary)
+{
+    console.log("winner is jonh")
+
+}
+else if (mike>john && mike>mary)
+{
+    console.log("winner is Mike")
+
+}
+else if (mary>john && mary>mike)
+{
+    console.log("winner is mary")
+
+}
+else{
+    console.log("this is wrong")
+}
