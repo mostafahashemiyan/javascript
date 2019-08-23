cc4221eca2333d3d43b5423bde75a7e84c94b7dc
mostafahashemiyan <49098593+mostafahashemiyan@users.noreply.github.com>
2019-08-23 11:27:55 +0430

    Add files via upload

---
+++ project1/script.js
@@ -0,0 +1,19 @@
+
+var nameJohn="john"
+var johnHeight = 1.7
+var johnMass = 75
+var nameMark="mark"
+var markHeight = 1.85
+var markMass = 90
+var johnBMI= johnMass/(johnHeight*johnHeight)
+var markBMI= markMass/(markHeight*markHeight)
+console.log(johnHeight)
+console.log(nameJohn +" "+" Height is"+johnHeight+"john mass is"+johnMass)
+console.log(nameMark +" "+" Height is"+markHeight+"mark mass is"+markMass)
+console.log(johnBMI)
+console.log(markBMI)
+
+var boolean= johnBMI > markBMI
+console.log(boolean)
+
+console.log("john BMI is grater than Mark ? "+boolean)
